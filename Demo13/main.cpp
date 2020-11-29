0aab223ab71979fd06e9403632106436f282a6a6
songwei163 <songweicpp@163.com>
2020-11-29 16:04:20 +0800

    图像几何变换

---
+++ Demo13/main.cpp
@@ -0,0 +1,93 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <imgproc/types_c.h>
+
+using namespace cv;
+using namespace std;
+
+// 不改变图像大小
+Mat imgTranslate(Mat& srcImg, int xOffset, int yOffset);
+// 改变图像大小
+Mat imgTranslate2(Mat& srcImg, int xOffset, int yOffset);
+
+int main()
+{
+	Mat img = imread("girl.jpg");
+	Mat dst;
+	// 缩小
+	// resize(img, dst, Size(), 0.5, 0.5, CV_INTER_AREA);
+	// imshow("dst", dst);
+
+	// 平移
+	// 不改变图像大小
+	// dst = imgTranslate(img, 100, 100);
+	// 改变图像大小
+	// dst = imgTranslate2(img, 100, 100);
+	// imshow("dst", dst);
+
+	/*transpose(img, dst);
+	flip(dst, dst, 0);*/
+
+	int rows = img.rows;
+	int cols = img.cols;
+
+	Mat xMapImg = Mat::zeros(img.size(), CV_32FC1);  //map1
+	Mat yMapImg = Mat::zeros(img.size(), CV_32FC1);  //map2
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			xMapImg.at<float>(i, j) = j;  //保持列不变
+			//yMapImg.at<float>(i, j) = i + 5 * sin(j/10.0);
+			yMapImg.at<float>(i, j) = rows - i;
+		}
+	}
+	remap(img, dst, xMapImg, yMapImg, CV_INTER_LINEAR);
+
+	imshow("img", img);
+	imshow("dst", dst);
+	
+	waitKey(0);
+	return 0;
+}
+
+Mat imgTranslate(Mat& srcImg, int xOffset, int yOffset)
+{
+	int rows = srcImg.rows;
+	int cols = srcImg.cols;
+	Mat dstImg = Mat::zeros(srcImg.size(), srcImg.type());  //创建全黑图像
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			int x = j + xOffset;
+			int y = i + yOffset;
+			if (x >= 0 && y >= 0 && x < cols && y < rows)
+			{
+				dstImg.at<Vec3b>(y, x) = srcImg.at<Vec3b>(i, j);
+			}
+		}
+	}
+	return dstImg;
+}
+
+Mat imgTranslate2(Mat& srcImg, int xOffset, int yOffset)
+{
+	int rows = srcImg.rows + yOffset;
+	int cols = srcImg.cols + xOffset;
+	Mat dstImg = Mat::zeros(rows, cols, srcImg.type());  //创建全黑图像
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			int x = j + xOffset;
+			int y = i + yOffset;
+			if (x >= 0 && y >= 0 && x < cols && y < rows)
+			{
+				dstImg.at<Vec3b>(y, x) = srcImg.at<Vec3b>(i, j);
+			}
+		}
+	}
+	return dstImg;
+}
